242b374e62fb28aa483c6a0ab88cd65daa9fa8b2
anakinzhed <norys07ss@gmail.com>
2024-10-10 14:53:25 -0600

    env_variable_fix

--- readme.md
+++ readme.md
@@ -1,7 +1,5 @@
 # Rust CLI Tool - Training Session
 ### This program sends a specified amount of uosmo to a destination wallet
-
-* Add dependencies in your `Cargo.toml` file.
 * Compile your project: `cargo build`
 * For documentation, run: `cargo doc --open`
 
@@ -9,7 +7,11 @@
 1. Go to [Levana Testnet](https://testnet-trade.levana.finance/).
 2. Connect a wallet:
    - I recommend **Keplr**. Set up the wallet.
-   - Save the wallet mnemonic in the root of this tool in the path `wallet/wallet.key`.
+   - Now you need to create an environment variable named `mnemonic`.    
+     This variable should contain the secret key for your wallet.
+     ```bash
+     export mnemonic="place your secret wallet key or wont be valid for the program"
+     ```
    - Click on the wallet address and you will see the Faucet.
      > **Note**: A faucet in the blockchain is a tool or service that distributes small amounts of cryptocurrency for free to users (training purposes).
 3. Complete the security challenge and get some free funds.
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
+/// SeedPhrase::from_str
 use std::str::FromStr;
 
-// File I/O for better error handling
-use fs_err;
-
-// Error handling
+/// Error handling
 use anyhow::{anyhow, Context, Result};
 
-// Json Response
+/// Json Response
 use serde_json::json;
 
-// Parse input
+/// Parse input
 use clap::Parser;
-
-#[derive(Parser, Debug)]
-struct Opt {
-    /// Path to the Cosmos configuration file
-    #[clap(env = "wallet_mnemonic", global = true)] // Captura desde la variable de entorno
-    mnemonic: String,
-}
 
 #[derive(Parser, Debug)]
 struct Transaction {
@@ -25,10 +16,16 @@ struct Transaction {
     coin: cosmos::ParsedCoin,
     /// Destination address to receive the funds
     destination: cosmos::Address,
+    /// Capture environment variable mnemonic
+    #[clap(env = "mnemonic")]
+    origin: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Init subscriber to handle traces
+    tracing_subscriber::fmt::init();
+
     tracing::info!("Rust Cli Tool has started");
 
     // If some wrong format is detected will panic
@@ -56,38 +53,40 @@ async fn main() -> Result<()> {
     }
 }
 
-/// Executes a transaction on the Cosmos blockchain.
+/// Executes a transaction on the Osmosis Testnet.
 ///
 /// This function performs the following steps:
-/// 1. Connects to the Cosmos blockchain.
-/// 2. Retrieves the balance for the given Cosmos wallet address.
-/// 3. Loads the wallet from the file located at `wallet/wallet.key`.
-/// 4. Executes the specified transaction by sending the specified token amount to the destination address.
+/// 1. Connects to Osmosis Testnet
+/// 2. Retrieves the balance for the provided address.
+/// 3. Loads a wallet using a mnemonic phrase obtained from the `transaction.origin` field.
+/// 4. Sends the specified token amount to the destination address using the provided transaction details.
 ///
 /// ### Arguments
-/// * `transaction` - A reference to a [`Transaction`] object containing the transaction details,
-///   including the recipient address, token, and amount to be transferred.
+/// * `transaction` - A reference to a [`Transaction`] struct containing the transaction details:
+///   - `coin`: The amount to transfer, in the form of a [`ParsedCoin`], e.g., "110uosmo".
+///   - `destination`: The wallet address that will receive the funds.
+///   - `origin`: The mnemonic phrase for the wallet from which the funds will be sent, captured from the environment variable `mnemonic`.
 ///
 /// ### Returns
-/// Returns a tuple containing:
-/// * `TxResponse::code` - A `u32` representing the transaction response code (0 for success, non-zero for failure).
-/// * `TxResponse::height` - An `i64` representing the block height at which the transaction was included.
-/// * `TxResponse::tx_hash` - A `String` representing the transaction hash, useful for tracking the transaction.
+/// Returns a `serde_json::Value` containing:
+/// * `code` - A `u32` representing the transaction response code (0 indicates success, non-zero indicates failure).
+/// * `height` - An `i64` representing the block height where the transaction was included.
+/// * `tx_hash` - A `String` representing the transaction hash, useful for tracking the transaction on the blockchain.
 ///
 /// ### Errors
 /// This function may return an error in the following cases:
-/// - If it fails to connect to the Cosmos blockchain.
-/// - If the `wallet.key` file does not exist or cannot be read.
-/// - If the mnemonic phrase in the `wallet.key` file is invalid or cannot be loaded.
-/// - If the transaction fails during execution.
-///
+/// - If there is a failure connecting to the Cosmos blockchain
+/// - If the balance retrieval for the provided address fails
+/// - If the mnemonic phrase provided in the `origin` field is invalid or cannot be parsed
+/// - If the transaction execution fails
+
 async fn execute_transaction(transaction: &Transaction) -> Result<serde_json::Value> {
     // Connect to the blockchain
-    tracing::info!("Connecting to Cosmos...");
+    tracing::info!("Connecting to Osmosis Testnet...");
     let cosmos_addr = cosmos::CosmosNetwork::OsmosisTestnet
         .connect()
         .await
-        .context("Error connecting to Cosmos")?;
+        .context("Error connecting to Osmosis Testnet")?;
     tracing::info!("Connection successful.");
 
     // Get the address
@@ -96,6 +95,7 @@ async fn execute_transaction(transaction: &Transaction) -> Result<serde_json::Va
     // Get balance
     tracing::info!("Getting balance for wallet {}", address);
 
+    // Get all balances
     let balances = cosmos::Cosmos::all_balances(&cosmos_addr, address)
         .await
         .context("Failed to retrieve all balances for the Cosmos address")?;
@@ -117,21 +117,15 @@ async fn execute_transaction(transaction: &Transaction) -> Result<serde_json::Va
     let amount: Vec<cosmos::Coin> = vec![coin];
 
     // Load the wallet
-    // Check if wallet exists in environment variable
-    // check if work without parse
-    let wallet = Opt::parse();
+    // Get mnemonic from environment variable
+    let mnemonic = transaction.origin.clone();
 
-    // Get the wallet
-    // Read the wallet key
-    let wallet_key = fs_err::read_to_string(wallet.mnemonic)
-        .context("Error reading the wallet.key file")?;
-
-    // Get Mnemonicclear
-    let mnemonic = cosmos::SeedPhrase::from_str(&wallet_key)
+    // Get Mnemonic
+    let seedphrase = cosmos::SeedPhrase::from_str(&mnemonic)
         .context("Failed to retrieve the mnemonic phrase")?;
 
     // Get wallet from Mnemonic
-    let wallet = mnemonic
+    let wallet = seedphrase
         .with_hrp(cosmos::AddressHrp::from_static("osmo"))
         .context("Error identifying the wallet")?;
 
